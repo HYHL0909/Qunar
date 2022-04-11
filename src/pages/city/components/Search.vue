42a999e7faae2d40c3f9849fc49bfbd862ae616c
Elvira <2291517651@qq.com>
2022-04-11 21:12:01 +0800

    city-search-bar

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="search">
+        <input  class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl';
+  .search
+    height: .72rem
+    background-color $bgColor
+    padding: 0 .1rem
+    .search-input
+      color:#222
+      width:100%
+      text-align: center
+      height: .62rem
+      line-height: .62rem
+      border-radius: .06rem   
+</style>
